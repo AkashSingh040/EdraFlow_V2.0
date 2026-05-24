6ae33c6645f444cbf5fbbe22b61303cd0d0e4f5f
akash <ak96908266@gmail.com>
2026-05-24 09:54:56 +0530

    deploy mistakes correct

--- rag/main.py
+++ rag/main.py
@@ -37,7 +37,7 @@ app = FastAPI(
 # ── CORS ─────────────────────────────────────────────────────────────────────
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173", "https://edra-flow-v2-0-2w31qsepf-akashsingh040s-projects.vercel.app/"],
+    allow_origins=["http://localhost:5173", "https://edra-flow-v2-0-2w31qsepf-akashsingh040s-projects.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
